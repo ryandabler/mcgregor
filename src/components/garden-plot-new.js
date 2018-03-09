e9363bd87b7d7892f0adf8f1d7618704c6541ca4
Ryan Dabler <rpdabler@gmail.com>
2018-03-09 13:20:06 -0700

    add placeholder element for creating new garden plot

---
+++ src/components/garden-plot-new.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import "./garden-plot-new.css";
+
+export default function GardenPlotNew() {
+    return (
+        <div className="garden-plot-new">
+            Add New Crop
+        </div>
+    );
+}
