7eef42666e5736b58147cf242413b7e6ca557dcb
Ryan Dabler <rpdabler@gmail.com>
2018-03-10 15:31:01 -0700

    route to new garden crop form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import Garden from "./components/garden";
+import GardenPlotNewForm from "./components/garden-plot-new-form";
 
 import './App.css';
 
@@ -13,6 +14,7 @@ export function App(props) {
       <div className="App">
         <Switch>
           <Route exact path="/" component={Garden} />
+          <Route exact path="/garden/new" component={GardenPlotNewForm} />
         </Switch>
       </div>
     </Router>
