41a6a68a0d8e9a17f88713f871d1b8d979ddcc00
Ryan Dabler <rpdabler@gmail.com>
2018-03-10 17:24:31 -0700

    use input names for populating cropValues

--- src/components/garden-plot-new-form.js
+++ src/components/garden-plot-new-form.js
@@ -16,8 +16,8 @@ export function GardenPlotNewForm(props) {
         
         const cropValues = {};
         Object.keys(e.target.elements).forEach(key => {
-            const id = e.target.elements[key].id;
-            if (id) cropValues[id] = e.target.elements[key].value;
+            const name = e.target.elements[key].name;
+            if (name) cropValues[name] = e.target.elements[key].value;
         });
 
         props.dispatch(addNewCrop(cropValues));
@@ -28,31 +28,31 @@ export function GardenPlotNewForm(props) {
             <h2>Add New Crop</h2>
             <div className="new-crop-form-row">
                 <label htmlFor="newCropName">Name</label>
-                <input id="newCropName" type="text" required />
+                <input id="newCropName" name="name" type="text" required />
             </div>
             <div className="new-crop-form-row">
                 <label htmlFor="newCropPlantDate">Plant date</label>
-                <input id="newCropPlantDate" type="date" required />
+                <input id="newCropPlantDate" name="plant_date" type="date" required />
             </div>
             <div className="new-crop-form-row">
                 <label htmlFor="newCropGermDays">Days to germination</label>
-                <input id="newCropGermDays" type="number" required />
+                <input id="newCropGermDays" name="germination_days" type="number" required />
             </div>
             <div className="new-crop-form-row">
                 <label htmlFor="newCropHarvDays">Days to harvest</label>
-                <input id="newCropHarvDays" type="number" required />
+                <input id="newCropHarvDays" name="harvest_days" type="number" required />
             </div>
             <div className="new-crop-form-row">
                 <label htmlFor="newCropPlantDepth">Planting depth</label>
-                <input id="newCropPlantDepth" type="number" step=".01" />
+                <input id="newCropPlantDepth" name="planting_depth" type="number" step=".01" />
             </div>
             <div className="new-crop-form-row">
                 <label htmlFor="newCropRowSpace">Row spacing</label>
-                <input id="newCropRowSpace" type="number" step=".01" />
+                <input id="newCropRowSpace" name="row_spacing" type="number" step=".01" />
             </div>
             <div className="new-crop-form-row">
                 <label htmlFor="newCropSeedSpace">Seed spacing</label>
-                <input id="newCropSeedSpace" type="number" step=".01" />
+                <input id="newCropSeedSpace" name="seed_spacing" type="number" step=".01" />
             </div>
             <input type="submit" />
             <button onClick={cancelNewCrop}>Cancel</button>
