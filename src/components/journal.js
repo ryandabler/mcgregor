d5dc5f6cb9dc097bbba9c518ae416a3dedaa1d63
Ryan Dabler <rpdabler@gmail.com>
2018-03-12 16:55:22 -0600

    overhaul journal logic

--- src/components/journal.js
+++ src/components/journal.js
@@ -1,18 +1,36 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import JournalEntry from "./journal-entry";
+import JournalYear from "./journal-year";
+import { getJournalYears, getJournalEntriesByYear, sortJournalByDate } from "../utilities";
 
 import "./journal.css";
 
 export function Journal(props) {
-    const entries = props.entries.map(entry => <JournalEntry key={entry.id} 
-                                                             date={entry.date}
-                                                             scope={entry.scope}
-                                                             text={entry.text} />);
+    function toggleDisplay(year) {
+        const journals = Array.from(document.getElementsByClassName("journal-year"));
+        journals.forEach(journal => {
+            if (journal.getElementsByClassName("journal-header")[0].getAttribute("data-year") === year.toString()) {
+                journal.getElementsByClassName("journal-entries")[0].classList.toggle("hidden");
+            } else {
+                journal.getElementsByClassName("journal-entries")[0].classList.add("hidden");
+            }
+        })
+    }
+
+    const years = getJournalYears(props.entries);
+    const journalByYear = years.map(year => 
+        sortJournalByDate(getJournalEntriesByYear(props.entries, year))
+    );
+    const groups = years.map((year, idx) => <JournalYear key={year}
+                                                         year={year}
+                                                         entries={journalByYear[idx]}
+                                                         toggle={toggleDisplay} />);
+
+    
     return (
         <div className="journal">
-            {entries}
+            {groups}
         </div>
     );
 }
