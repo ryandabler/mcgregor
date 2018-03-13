ce1dd95f0f2fcd498c423639e00c14d6414d0ab0
Ryan Dabler <rpdabler@gmail.com>
2018-03-13 10:01:04 -0600

    set Route for GardenPlotDetail

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { connect } from "react-redux";
 
 import Garden from "./components/garden";
 import GardenPlotNewForm from "./components/garden-plot-new-form";
+import GardenPlotDetails from "./components/garden-plot-detail";
 
 import './App.css';
 
@@ -14,6 +15,7 @@ export function App() {
         <Switch>
           <Route exact path="/" component={Garden} />
           <Route exact path="/garden/new" component={GardenPlotNewForm} />
+          <Route exact path="/garden/:id" component={GardenPlotDetails} />
         </Switch>
       </div>
     </Router>
