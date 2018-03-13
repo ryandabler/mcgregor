566b5751195f4f1cb3d9afcab1f10b9d14f99064
Ryan Dabler <rpdabler@gmail.com>
2018-03-13 10:00:09 -0600

    App no longer has props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import PropTypes from "prop-types";
 import { connect } from "react-redux";
 
 import Garden from "./components/garden";
@@ -8,7 +7,7 @@ import GardenPlotNewForm from "./components/garden-plot-new-form";
 
 import './App.css';
 
-export function App(props) {
+export function App() {
   return (
     <Router>
       <div className="App">
@@ -21,17 +20,4 @@ export function App(props) {
   );
 }
 
-App.defaultProps = {
-  gardens: []
-}
-
-App.propTypes = {
-  gardens: PropTypes.arrayOf(PropTypes.object)
-}
-
-const mapStateToProps = state => ({
-  email: state.email,
-  gardens: state.gardens
-});
-
-export default connect(mapStateToProps)(App);
+export default connect()(App);
