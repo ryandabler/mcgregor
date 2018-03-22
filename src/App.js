c3644d6dbee04b0771a77062b247f94dbff10ca5
Ryan Dabler <rpdabler@gmail.com>
2018-03-22 10:31:54 -0600

    implement landing page and related routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { connect } from "react-redux";
 
+import LandingPage from "./components/landing-page";
 import Garden from "./components/garden";
 import GardenPlotNewForm from "./components/garden-plot-new-form";
 import GardenPlotDetails from "./components/garden-plot-detail";
@@ -13,7 +14,8 @@ export function App() {
 			<Router>
 				<div className="App">
 					<Switch>
-						<Route exact path="/" component={Garden} />
+						<Route exact path="/" component={LandingPage} />
+						<Route exact path="/garden" component={Garden} />
 						<Route exact path="/garden/new" component={GardenPlotNewForm} />
 						<Route exact path="/garden/:id" component={GardenPlotDetails} />
 					</Switch>
