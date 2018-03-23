7f862ee3efc9a60e707eeaa0953127b9b71320ca
Ryan Dabler <rpdabler@gmail.com>
2018-03-23 09:06:15 -0600

    create dynamic API_BASE_URL in config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ||
-    "http://localhost:3000/api";
+const API_BASE_URL = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_BASE_URL
+    : "http://localhost:8080";
 
 module.exports = { API_BASE_URL };
