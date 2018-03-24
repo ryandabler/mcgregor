2be8b0047bc58a06be5f250c1b97b929465b921e
Ryan Dabler <rpdabler@gmail.com>
2018-03-24 09:21:08 -0600

    export store

--- src/store.js
+++ src/store.js
@@ -2,7 +2,9 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { gardenReducer } from "./reducer";
 
-export default createStore(
+const store = createStore(
     gardenReducer,
     applyMiddleware(thunk)
 );
+
+export default store;
