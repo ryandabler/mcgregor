29da21b4f55e48665217e92089278b0188ebc751
Ryan Dabler <rpdabler@gmail.com>
2018-03-29 10:14:18 -0600

    improve date creation from strings

--- src/utilities.js
+++ src/utilities.js
@@ -77,3 +77,9 @@ export function queryServer(method, endpoint, authToken, body = undefined) {
     .then(res => normalizeResponseErrors(res))
     .catch(err => console.log(err));
 }
+
+export function makeDateFromISOString(isoDate = (new Date()).toISOString()) {
+    const date = isoDate.split("T")[0];
+    const [ year, month, day ] = date.split("-");
+    return new Date(year, month - 1, day).toLocaleDateString();
+}
